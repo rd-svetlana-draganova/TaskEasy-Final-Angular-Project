b95de77433f4eeb12df77312ef6eaa4c9f816b40
Svetlana Draganova <svetlana.draganova@rewe-group.com>
2026-01-16 10:04:58 +0200

    feat: Added the tasks to the board

--- src/app/task/taskservice.ts
+++ src/app/task/taskservice.ts
@@ -1,23 +1,13 @@
 import {HttpClient} from '@angular/common/http';
-import {tap} from 'rxjs';
 import {Task} from '../models/task.model';
+import {Injectable} from '@angular/core';
 
+@Injectable({ providedIn: 'root' })
 export class TaskService {
-
-  private tasks: Task[] = [];
 
   constructor(private readonly http: HttpClient) {}
 
   loadTasks() {
-    return this.http.get<Task[]>('/public/task.json')
-      .pipe(tap(data => this.tasks = data));
-  }
-
-  getTasksByStatus(status: Task['status']) {
-    return this.tasks.filter(task => task.status === status)
-  }
-
-  getTaskById(id: number) {
-    return this.tasks.find(task => task.id === id);
+    return this.http.get<Task[]>('assets/tasks.json');
   }
 }
