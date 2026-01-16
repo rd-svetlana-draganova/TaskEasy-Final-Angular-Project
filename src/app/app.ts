19b4ab4cbf84f3e29726d11e6504e143136b2a10
Svetlana Draganova <svetlana.draganova@rewe-group.com>
2026-01-16 14:47:42 +0200

    feat: added task routing and polished the UI layout of the task

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Board} from './board/board';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Board],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('TaskEasy-Final-Angular-Project');
