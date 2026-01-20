b517bbb74252b316cd4e54587ff89c3f67733f0d
Svetlana Draganova <svetlana.draganova@rewe-group.com>
2026-01-20 10:30:07 +0200

    feat: Added filters and background (#11)

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,11 +7,12 @@ import { Router, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Authservice } from '../services/authservice';
 import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CommonModule, RouterModule, DragDropModule],
+  imports: [CommonModule, RouterModule, DragDropModule, FormsModule],
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
@@ -19,6 +20,11 @@ export class BoardComponent implements OnInit {
   toDoList: Task[] = [];
   inProgressList: Task[] = [];
   doneList: Task[] = [];
+  assignedToFilter: string = '';
+  estimationFilter: string = '';
+  filteredToDoList: Task[] = [];
+  filteredInProgressList: Task[] = [];
+  filteredDoneList: Task[] = [];
 
   constructor(
     private readonly taskService: TaskService,
@@ -30,16 +36,50 @@ export class BoardComponent implements OnInit {
   ngOnInit() {
     this.taskService.getTaskByStatus('to-do').subscribe(tasks => {
       this.toDoList = tasks;
+      this.applyFilters();
       this.cd.detectChanges();
     });
     this.taskService.getTaskByStatus('in-progress').subscribe(tasks => {
       this.inProgressList = tasks;
+      this.applyFilters();
       this.cd.detectChanges();
     });
     this.taskService.getTaskByStatus('done').subscribe(tasks => {
       this.doneList = tasks;
+      this.applyFilters();
       this.cd.detectChanges();
     });
+  }
+
+  applyFilters() {
+    let estimationValue = this.determineEstimationFilterValue(this.estimationFilter);
+
+    this.filteredToDoList = this.toDoList.filter(task =>
+      (this.assignedToFilter === '' || task.assignedTo === this.assignedToFilter) &&
+      (estimationValue === '' || task.estimation === estimationValue)
+    );
+    this.filteredInProgressList = this.inProgressList.filter(task =>
+      (this.assignedToFilter === '' || task.assignedTo === this.assignedToFilter) &&
+      (estimationValue === '' || task.estimation === estimationValue)
+    );
+    this.filteredDoneList = this.doneList.filter(task =>
+      (this.assignedToFilter === '' || task.assignedTo === this.assignedToFilter) &&
+      (estimationValue === '' || task.estimation === estimationValue)
+    );
+  }
+
+  private determineEstimationFilterValue(value: string): string {
+    const validValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+    if (validValues.includes(value)) return value;
+    return '';
+  }
+
+  applyAssignedToFilter() {
+    this.applyFilters();
+  }
+
+  applyEstimationFilter() {
+    this.applyFilters();
   }
 
   openTask(taskId: string) {
